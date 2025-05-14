76cc7b1b8152969f90955568e1a5d33ba2335f31
advaithsuryaP <padvaithsurya@gmail.com>
2025-05-14 00:17:56 -0400

    Update sign-in component: reset default values for email and password fields in the sign-in form to enhance security and user experience.

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -43,11 +43,11 @@ export class AuthSignInComponent {
         message: ''
     };
     signInForm = new FormGroup({
-        email: new FormControl('kevinf@umbc.edu', {
+        email: new FormControl('', {
             validators: [Validators.required, Validators.email],
             nonNullable: true
         }),
-        password: new FormControl('admin', { validators: [Validators.required], nonNullable: true }),
+        password: new FormControl('', { validators: [Validators.required], nonNullable: true }),
         rememberMe: new FormControl(false, { nonNullable: true })
     });
     showAlert: boolean = false;
